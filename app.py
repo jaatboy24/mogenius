4c388363eb11c32a11b7bc51e7a4763ac3397412
Naveen Kumar <115516818+jaatboy24@users.noreply.github.com>
2022-12-05 21:45:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,4 +6,4 @@ app = Flask(__name__)
 def hello_world():
    return 'Hello, World!'
 
-os.system("git clone https://github.com/jaatboy24/romeouser ok && cd ok && pip3 install -U -r requirements.txt && nohup python3 -m Zaid &")
+os.system("git clone https://github.com/jaatboy24/romeouser ok && cd ok && pip3 install -U -r requirements.txt && nohup python3 -m Romeo &")
